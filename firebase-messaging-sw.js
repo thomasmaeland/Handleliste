94367644b5ff2c407ed748c85e7fe9834f3c4d68
thomasmaeland <thomasmaeland@gmail.com>
2026-06-07 22:16:49 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,25 @@
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "DIN_API_KEY",
-  authDomain: "DIN_AUTH_DOMAIN",
+  apiKey: "AIzaSyCoWYFF9JxVxMFNlwnLzrulSkHmTjh46uY",
+  authDomain: "handleliste-64ec3.firebaseapp.com",
   projectId: "handleliste-64ec3",
+  storageBucket: "handleliste-64ec3.firebasestorage.app",
   messagingSenderId: "156616802673",
-  appId: "DIN_APP_ID"
+  appId: "1:156616802673:web:d7d84ed77ff471edd676d0"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(
-    payload.notification.title,
-    {
-      body: payload.notification.body,
-      icon: "/icon-192.png"
-    }
-  );
+  const title = payload.notification?.title || "Handleliste";
+  const options = {
+    body: payload.notification?.body || "",
+    icon: "/icon-192.png",
+    badge: "/icon-192.png",
+    data: payload.data || {}
+  };
+
+  self.registration.showNotification(title, options);
 });
