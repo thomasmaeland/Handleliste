c74dfaf44b89849a6d61241d00b94af33188d52a
thomasmaeland <thomasmaeland@gmail.com>
2026-06-07 22:08:30 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+  apiKey: "DIN_API_KEY",
+  authDomain: "DIN_AUTH_DOMAIN",
+  projectId: "handleliste-64ec3",
+  messagingSenderId: "156616802673",
+  appId: "DIN_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/icon-192.png"
+    }
+  );
+});
